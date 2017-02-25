37ddf1671e831bd6f88df372a42d44b07c4a17a9
KaMaL <KaMaL@KAMAL>
2017-02-25 20:33:04 +0200

    Department

--- App_Code/DepartmentLayer.cs
+++ App_Code/DepartmentLayer.cs
@@ -13,9 +13,38 @@ public static class DepartmentLayer
 {
     public static DataSet SelectAllDepartments()
     {
+        string s = @"SELECT Department.*,Instructor.Ins_Name
+            FROM Department
+            JOIN Instructor ON Department.Mgr_Id = Instructor.Ins_Id";
+        return DataAccessLayer.SelectCommand(s);
+    }
+    public static DataSet SelectAllDepartmentsWithoutMgr()
+    {
         string s = @"SELECT *
             FROM Department";
         return DataAccessLayer.SelectCommand(s);
     }
-
+    public static int InsertDepartment(string Dept_Name, int? Mgr_Id)
+    {
+        string s = "Insert_Department";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("@Dept_Name", Dept_Name),
+            new SqlParameter("@Mgr_Id", Mgr_Id??SqlInt32.Null)
+        };
+        return DataAccessLayer.DMLCommandSP(s, p);
+    }
+    public static int UpdateDepartment(int Dept_Id,string Dept_Name, int? Mgr_Id)
+    {
+        string s = "Update_Department";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("@Dept_Id", Dept_Id),
+            new SqlParameter("@Dept_Name", Dept_Name),
+            new SqlParameter("@Mgr_Id", Mgr_Id??SqlInt32.Null)
+        };
+        return DataAccessLayer.DMLCommandSP(s, p);
+    }
+    public static int DeleteDepartment(int Dept_Id)
+    {
+        string s = "Delete_Department";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("@Dept_Id", Dept_Id) };
+        return DataAccessLayer.DMLCommandSP(s, p);
+    }
 }
--- DepartmentPage.aspx.cs
+++ DepartmentPage.aspx.cs
@@ -5,10 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class DepartmentPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void InsertBtn_Click(object sender, EventArgs e)
+    {
+        if (IsValid)
+        {
+            DepartmentSource.InsertParameters["St_Name"].DefaultValue = NameTxt.Text;
+            DepartmentSource.InsertParameters["St_Age"].DefaultValue = AgeTxt.Text;
+            DepartmentSource.InsertParameters["St_Address"].DefaultValue = AddressTxt.Text;
+            DepartmentSource.InsertParameters["Dept_Id"].DefaultValue = DepartmentList.SelectedValue;
+            DepartmentSource.Insert();
+        }
     }
 }
