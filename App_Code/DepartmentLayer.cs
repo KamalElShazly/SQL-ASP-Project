949a6cf62f1a6775e2a77941b3db437b0ca526d0
KaMaL <KaMaL@KAMAL>
2017-02-28 18:57:32 +0200

    Department zeft

--- App_Code/DepartmentLayer.cs
+++ App_Code/DepartmentLayer.cs
@@ -27,7 +27,8 @@ public static class DepartmentLayer
     {
         string s = @"SELECT Department.Dept_Id,Department.Dept_Name,Instructor.Ins_Name
                     FROM Department 
-                    FULL JOIN Instructor ON Department.Mgr_Id =Instructor.Ins_Id and Department.Dept_Id=" + Dept_Id.ToString();
+                    FULL JOIN Instructor ON Department.Mgr_Id =Instructor.Ins_Id
+                    WHERE Department.Dept_Id=" + Dept_Id.ToString();
         return DataAccessLayer.SelectCommand(s);
     }
     public static int InsertDepartment(string Dept_Name, int? Mgr_Id)
