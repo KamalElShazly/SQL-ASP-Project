4b2d47bcf4f3550623b79d1122f7156bd7af02c5
KaMaL <KaMaL@KAMAL>
2017-02-28 18:48:31 +0200

    Department

--- App_Code/DepartmentLayer.cs
+++ App_Code/DepartmentLayer.cs
@@ -26,8 +26,8 @@ public static class DepartmentLayer
     public static DataSet SelectDepartmentById(int Dept_Id)
     {
         string s = @"SELECT Department.Dept_Id,Department.Dept_Name,Instructor.Ins_Name
-                    FROM Instructor
-                    FULL JOIN Department ON Department.Mgr_Id =Instructor.Ins_Id and Department.Dept_Id=" + Dept_Id.ToString();
+                    FROM Department 
+                    FULL JOIN Instructor ON Department.Mgr_Id =Instructor.Ins_Id and Department.Dept_Id=" + Dept_Id.ToString();
         return DataAccessLayer.SelectCommand(s);
     }
     public static int InsertDepartment(string Dept_Name, int? Mgr_Id)
---
+++ GenerateExam.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage1.master" AutoEventWireup="true" CodeFile="GenerateExam.aspx.cs" Inherits="GenerateExam" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Repeater ID="Repeater1" runat="server">
+    </asp:Repeater>
+</asp:Content>
+
