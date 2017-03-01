6d55af5f81c6355031b5375255e8b5f3112954f3
KaMaL <KaMaL@KAMAL>
2017-03-01 21:08:22 +0200

    Exam

--- GenerateExam.aspx.cs
+++ GenerateExam.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class GenerateExam : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    }
 
+    protected void GenerateBtn_Click(object sender, EventArgs e)
+    {
+        DataView Questions = (DataView)ExamSource.Select();
+        foreach(DataRowView Question in Questions)
+        {
+            TableRow tRow = new TableRow();
+            foreach (string q in Question.Row.ItemArray)
+            {
+                TableCell tCell = new TableCell();
+                tCell.Text = q;
+                tRow.Cells.Add(tCell);
+            }
+            Table1.Rows.Add(tRow);
+        }
     }
 }
