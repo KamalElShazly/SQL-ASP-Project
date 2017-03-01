6fb27c600278d05cb65a32ac71b239c54a8d207a
Engy <Engy@DESKTOP-8AF7NIO>
2017-03-01 21:42:38 +0200

    Corrective Per Course

--- App_Code/CourseLayer.cs
+++ App_Code/CourseLayer.cs
@@ -16,6 +16,11 @@ public static class CourseLayer
         string s = "Select * from Course";
         return DataAccessLayer.SelectCommand(s);
     }
+    public static DataSet SelectCourse_Id_Name()
+    {
+        string s = "Select Crs_Id,Crs_Name from Course";
+        return DataAccessLayer.SelectCommand(s);
+    }
     public static DataSet St_Results(int st_id)
     {
         string s = @"select C.Crs_Id,C.Crs_Name,SC.St_Grade
@@ -31,16 +36,19 @@ public static class CourseLayer
                      group by C.Crs_Id,C.Crs_Name";
         return DataAccessLayer.SelectCommand(s);
     }
+    public static DataSet Corrective_Per_Course(int Crs_Id)
+    {
+        string s = @"select distinct St_Ex_Q.St_Id ID,St_Name Name
+                     from St_Ex_Q ,Exam ,St_Ins_Crs ,Student
+                     where St_Ex_Q.St_Id=St_Ins_Crs.St_Id and St_Ex_Q.Ex_Id=Exam.Ex_Id 
+                     and Exam.Crs_Id=St_Ins_Crs.Crs_Id and St_Grade<Ex_Grade/2 and St_Ins_Crs.Crs_Id=" + Crs_Id;
+        return DataAccessLayer.SelectCommand(s);
+    }
     public static DataSet SelectCoursesPerStudent(int St_Id)
     {
         string s = @"SELECT Course.*
                     FROM Course
                     JOIN St_Ins_Crs ON Course.Crs_Id = St_Ins_Crs.Crs_Id and St_Ins_Crs.Crs_Id is not null and St_Id=" + St_Id.ToString();
-        return DataAccessLayer.SelectCommand(s);
-    }
-    public static DataSet SelectCourse_Id_Name()
-    {
-        string s = "Select Crs_Id,Crs_Name from Course";
         return DataAccessLayer.SelectCommand(s);
     }
 
