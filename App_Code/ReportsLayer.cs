2b53cba293dd9f80ee4fd1b77d38d79f9e2b5443
Sohaila Tarek <sohailatarekkhalifa@gmail.com>
2017-03-04 01:48:26 +0200

    report4

--- App_Code/ReportsLayer.cs
+++ App_Code/ReportsLayer.cs
@@ -29,4 +29,23 @@ public static class ReportsLayer
         SqlParameter[] p = new SqlParameter[] { new SqlParameter("Ins_Id", Ins_Id) };
         return DataAccessLayer.SelectCommandSP(s, p);
     }
+    public static DataSet TopicsPerCourse(int Crs_Id)
+    {
+        string s = @"Topics_per_Crs";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("Crs_Id", Crs_Id) };
+        return DataAccessLayer.SelectCommandSP(s, p);
+    }
+    public static DataSet QuestionsPerExam(int Ex_Id)
+    {
+        string s = @"Questions_Per_Exam";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("Ex_Id", Ex_Id) };
+        return DataAccessLayer.SelectCommandSP(s, p);
+    }
+    public static DataSet QuestionsPerExamWithStAns(int Ex_Id,int St_Id)
+    {
+        string s = @"QuestionsPerExam_With_St_Ans";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("Ex_Id", Ex_Id),
+        new SqlParameter("St_Id", St_Id)};
+        return DataAccessLayer.SelectCommandSP(s, p);
+    }
 }
