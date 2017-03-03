0563db9703bf4701c1a9581540565fce624c22c9
omar <omar@OMARHAMDY>
2017-03-03 18:50:05 +0000

    omar

---
+++ Student-Exam-ModelAnser.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Student_Exam_ModelAnser : System.Web.UI.Page
+{
+   protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            GridView2.SelectRow(0);
+        }
+    }
+
+    protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        GridView55.SelectRow(0);
+    }
+}
