9cf6c44b4409c4542b7691b4458e9e2706a68671
Sohaila Tarek <sohailatarekkhalifa@gmail.com>
2017-03-03 20:49:55 +0200

    reports

--- App_Code/ReportsLayer.cs
+++ App_Code/ReportsLayer.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 /// <summary>
 /// Summary description for ReportsLayer
 /// </summary>
-public class ReportsLayer
+public static class ReportsLayer
 {
-    public ReportsLayer()
+    public static DataSet StudentByDepartment(int Dept_Id)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        string s = @"St_info_according_DeptNo";
+        SqlParameter[] p = new SqlParameter[] { new SqlParameter("@Dept_Id", Dept_Id) };
+        return DataAccessLayer.SelectCommandSP(s,p);
     }
 }
--- Reports/Report1.aspx.cs
+++ Reports/Report1.aspx.cs
@@ -11,25 +11,15 @@ using CrystalDecisions.CrystalReports.Engine;
 
 public partial class Reports_Report1 : System.Web.UI.Page
 {
-    static SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ASPProject"].ToString());
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
 
-   
-
     protected void DropDownList1_SelectedIndexChanged1(object sender, EventArgs e)
     {
-        SqlCommand com = new SqlCommand("St_info_according_DeptNo", cn);
-        com.CommandType = CommandType.StoredProcedure;
-        com.Parameters.AddWithValue("@Dept_Id", DropDownList1.SelectedValue);
-        cn.Open();
-        SqlDataAdapter ad = new SqlDataAdapter();
-        ad.SelectCommand = com;
-        DataSet ds = new DataSet_Report1();
-        ad.Fill(ds);
-        cn.Close();
+        DataSet ds = ReportsLayer.StudentByDepartment(int.Parse(DropDownList1.SelectedValue));
+
         ReportDocument repdoc = new ReportDocument();
         repdoc.Load(Server.MapPath("Report1.rpt"));
         repdoc.SetDataSource(ds);
